f95aa8dc873279036939a41f5eba162a509d9e46
kalcalen <kalcalen@gmail.com>
2020-05-26 11:32:53 -0700

    Created functioning card counting game

--- functions.js
+++ functions.js
@@ -1,1 +1,49 @@
-// Your functions go here!
+// Counting cards function
+
+// Global variable, Will not be manipulated
+var count = 0;
+// Card parameter, will either increawe or descrease count variable
+function countingCards(card) {
+   // Checks card value, In the case of 2,3,4,5,6, Increment value
+   switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+         count++;
+         break;
+      // In the case of 10,J,Q,K,A, Decrement value
+      // In the case of 7,8,9 Decrement value as well
+      case 10:
+      case "J":
+      case "Q":
+      case "K":
+      case "A":
+         count--;
+         break;
+   }
+
+   // Variable that tells player to either "Hold" or "Bet"
+   var output = count;
+
+   // If count is more than 0, will return Bet
+   if (count > 0) {
+      output += " Bet";
+      // If count is negative, will return Hold
+   } else {
+      output += " Hold";
+   }
+
+   // Console log to return see counting results
+   console.log(output);
+
+   return output;
+}
+
+// Results of function. cc = card count
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
